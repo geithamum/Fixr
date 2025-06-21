8d563561b30cd29ac7302a8b91219da1fdefba50
geithamum <n.g.a.tgaming@gmail.com>
2025-06-21 07:35:13 -0600

    Deleted unnecessary files

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 from mcp_agent.app import MCPApp
 from mcp_agent.agents.agent import Agent
 from mcp_agent.workflows.llm.augmented_llm_openai import OpenAIAugmentedLLM
+from mcp_agent.workflows.llm.augmented_llm import RequestParams
 
 
 import asyncio
@@ -12,8 +13,9 @@ async def os_assistant():
         os_agent = Agent(
             name="windows_assistant",
             instruction="""You are an agent with access to full filesystem search capabilities on the user's computer with Everything Search, as well as the ability to execute commands on the user's computer using mcp-server-commands.
-            If the user asks to open a file, search for the file first, use a command to execute it, then tell the user what you did
-            If the user asks to open an application, search for either an executable (.exe) or a shortcut (.ink) file, use a command to execute it, then check the running processes to see if the application is running. If it is running, end the loop.""",
+            If the user asks to open a file, search for the file first, use a command to execute it, then generate a text response, don't call any more tools.
+            If the user asks to open an application, search for either an executable (.exe) or a shortcut (.ink) file, use a command to execute it. Always end the loop after executing the command whether it outputs something or not.
+            """,
             server_names=["fetch", "mcp-server-commands", "everything-search"]
         )
         
@@ -25,7 +27,7 @@ async def os_assistant():
                     break
                 else:
                     try:
-                        result = await llm.generate_str(user_input)
+                        result = await llm.generate(message=user_input, request_params=RequestParams(use_history=True, parallel_tool_calls=True, ))
                         print(result)
                     except Exception as e:
                         print(f"Error: {e}")
